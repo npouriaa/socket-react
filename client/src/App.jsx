6560958221fddd4e987dc3ef90427b221cf99cc9
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-10-25 20:11:22 +0330

    connecting backend & frontend and show message

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import { useEffect } from "react";
+
+const socket = io.connect("http://localhost:5174");
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  const [message, setMessage] = useState("");
+  const [messageReceived, setMessageReceived] = useState("");
+  const sendMessage = () => {
+    socket.emit("send_message", {
+      message: message,
+    });
+  };
 
-export default App
+  useEffect(() => {
+    socket.on("receive_message", (data) => {
+      setMessageReceived(data.message);
+    });
+  }, [socket]);
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setMessage(e.target.value)}
+        type="text"
+        placeholder="message..."
+      />
+      <button onClick={() => sendMessage()}>Send message</button>
+      <h1>Message :</h1>
+      {messageReceived}
+    </div>
+  );
+};
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -10,11 +10,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "http://localhost:5173",
     methods: ["GET , POST"],
   },
 });
 
-server.listen(3001 , () => {
+io.on("connection" , (socket) => {
+  console.log(`user connected ${socket.id}`)
+
+  socket.on("send_message" , (data) => {
+    socket.broadcast.emit("receive_message" , data)
+  })
+})
+
+server.listen(5174 , () => {
     console.log('SERVER IS RUNNING')
 })
